54743cdda4c1e8539f8ae25bf41d2043d6ed6f4b
don11669 <48054558+don11669@users.noreply.github.com>
2019-03-21 19:54:04 +0800

    mySQL
    why keep using reserved word???

--- server/mysql-script/database-init.sql
+++ server/mysql-script/database-init.sql
@@ -5,19 +5,19 @@ use caritas_main;
 drop if exists table personnel;
 
 create table personnel
-(id varchar(64) primary key,Name varchar(128), Role int not null, Extra varchar(1024));
+(id varchar(64) primary key,p_name varchar(128), p_role int not null, Extra varchar(1024));
 
 create table master_record
-(id varchar(64) primary key,Location varchar(128),token varchar(128));
+(id varchar(64) primary key,location varchar(128),token varchar(128));
 
 create table record_form
-(id varchar(64) primary key,parent_token varchar(128),student_id varchar(64),data_json varchar(1024));--parent_token:the id at master_record
+(id varchar(64) primary key,parent_token varchar(128),student_id varchar(64),data_json varchar(1024)); /*parent_token:the id at master_record*/
 
 create table staff_location
-(id varchar(64) primary key, Location varchar(128))
+(id varchar(64) primary key, Location varchar(128));
 
-create table id_list--to ensure unique id
-(id varchar(64) primary key, used_id varchar(64), in_table varchar(128))
+create table id_list/*to ensure unique id*/
+(id varchar(64) primary key, used_id varchar(64), in_table varchar(128));
 
 /*
 data_json
@@ -26,9 +26,9 @@ store form data in json;
 
 create table record_child
 (id varchar(64) primary key, parent_token varchar(128),
-    student_id varchar(64), time datetime default now(), status int);
+    student_id varchar(64), record_time datetime default now(), child_status int);
 /*
-status :
+child_status :
 0: get in
 1: get out
 2: not sure
