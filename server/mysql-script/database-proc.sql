445ec877f627239f2a69def8695df2e5eb7bbbde
don11669 <48054558+don11669@users.noreply.github.com>
2019-03-24 22:09:38 +0800

    Update database-proc.sql

--- server/mysql-script/database-proc.sql
+++ server/mysql-script/database-proc.sql
@@ -1,8 +1,25 @@
 DELIMITER ';'
 uses caritas_main;
 /*drop procedures to update*/
+
 DELIMITER '/$'
 /*procedures to be added*/
 
+CREATE PROCEDURE gen_id()
+BEGIN
+set @id = md5(TO_BASE64(now()+rand()));
+  while (select count(*) from id_list where id_list.id = @id) > 0 do
+    set @id = md5(TO_BASE64(now()+rand()));
+  END while
+END
+/$
+
+
+
+
+
+
 
 DELIMITER ';'
+
+
