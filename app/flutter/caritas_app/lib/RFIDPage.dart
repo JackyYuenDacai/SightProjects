22645ea34de80eddf693d46ce4d2f949e8110791
Deepskyhunter <48083317+Deepskyhunter@users.noreply.github.com>
2019-04-04 00:08:03 +0800

    Update RFIDPage.dart

--- app/flutter/caritas_app/lib/RFIDPage.dart
+++ app/flutter/caritas_app/lib/RFIDPage.dart
@@ -4,21 +4,20 @@ import 'dart:async';
 import './enterList.dart';
 import './survey.dart';
 
-
 class Frame extends StatelessWidget {
-  final List colformList = [new ColForm(),new ColForm()];
+  final List colformList = [new ColForm(), new ColForm()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Toilet iPad"),), //replace with toilet location?
+      appBar: AppBar(
+        title: Text("Toilet iPad"),
+      ), //replace with toilet location?
       body: new Container(
-        child: new SingleChildScrollView (
-          scrollDirection: Axis.horizontal,
-          child: new Row(
-            children: colformList.map((element) => ColEntry())
-            //children:colform_list,
-          )
-        )),
+          child: new SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: new Row(children: colformList.map((element) => ColEntry())
+                  //children:colform_list,
+                  ))),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -26,31 +25,41 @@ class Frame extends StatelessWidget {
             UserAccountsDrawerHeader(
               accountName: new Text('Location'),
               currentAccountPicture: new CircleAvatar(
-                backgroundImage: AssetImage('images/pic1.jpg'), radius: 35.0), accountEmail: null,),
-            ListTile(title: Text('Manuel Page'),
-              leading: new CircleAvatar(child: new Text('B2'),),
+                  backgroundImage: AssetImage('images/pic1.jpg'), radius: 35.0),
+              accountEmail: null,
+            ),
+            ListTile(
+              title: Text('Manuel Page'),
+              leading: new CircleAvatar(
+                child: new Text('B2'),
+              ),
               onTap: () {
                 //Navigator.pop(context);
                 Navigator.pop(context);
-              },),
-            ListTile(title: Text('Settings'),
+              },
+            ),
+            ListTile(
+              title: Text('Settings'),
               leading: new CircleAvatar(
-                child: new Icon(Icons.list),),
+                child: new Icon(Icons.list),
+              ),
               onTap: () {
                 Navigator.pop(context);
-              },),
-              ListTile(title: Text('+ Test Student'),
+              },
+            ),
+            ListTile(
+                title: Text('+ Test Student'),
                 leading: new CircleAvatar(
-                  child: new Icon(Icons.list),),
+                  child: new Icon(Icons.list),
+                ),
                 onTap: () {
-                    colformList.add(new ColForm())
-                    ;
-                  })
+                  colformList.add(new ColForm());
+                })
           ],
         ),
-      ),);
+      ),
+    );
   }
-  
 }
 
 class RFIDPage extends StatefulWidget {
@@ -62,69 +71,74 @@ class RFIDPage extends StatefulWidget {
 }
 
 class _RFIDPageState extends State<RFIDPage> {
-  var subscription = Connectivity().onConnectivityChanged.listen((ConnectivityResult result) async{
-        var connectivityResult = await Connectivity().checkConnectivity();
-        if(connectivityResult != ConnectivityResult.mobile && connectivityResult != ConnectivityResult.wifi){
-              //DUMMY
-        }else{
-            //DUMMY
-        }
-      });
-  initState(){
+  var subscription = Connectivity()
+      .onConnectivityChanged
+      .listen((ConnectivityResult result) async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult != ConnectivityResult.mobile &&
+        connectivityResult != ConnectivityResult.wifi) {
+      //DUMMY
+    } else {
+      //DUMMY
+    }
+  });
+  initState() {
     super.initState();
-    var subscription = Connectivity().onConnectivityChanged.listen((ConnectivityResult result) async{
-          var connectivityResult = await Connectivity().checkConnectivity();
-          if(connectivityResult != ConnectivityResult.mobile && connectivityResult != ConnectivityResult.wifi){
-            //NO NETWORK CONNECTION
-            RFIDPage.networkConnection = false;
-          }else{
-            //HAVE NETWORK CONNECTION
-            RFIDPage.networkConnection = true;
-          }
-        });
+    var subscription = Connectivity()
+        .onConnectivityChanged
+        .listen((ConnectivityResult result) async {
+      var connectivityResult = await Connectivity().checkConnectivity();
+      if (connectivityResult != ConnectivityResult.mobile &&
+          connectivityResult != ConnectivityResult.wifi) {
+        //NO NETWORK CONNECTION
+        RFIDPage.networkConnection = false;
+      } else {
+        //HAVE NETWORK CONNECTION
+        RFIDPage.networkConnection = true;
+      }
+    });
   }
+
   @override
-  dispose(){
+  dispose() {
     subscription.cancel();
   }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return null;
   }
 }
+
 class ColEntry extends StatelessWidget {
-
   @override
-
   Widget build(BuildContext context) {
-    return       
-     new Container(
-          child: new Center(
-          child: new Column(
-            children: [
-              new SizedBox(height: 75),
-              new CircleAvatar(child: new Icon(Icons.school),radius: 70.0,),
-              new SizedBox(height: 35),
-              new Text(
-                element,
-                textAlign:TextAlign.center, 
-                textScaleFactor:2.0,
-              ),
-              new SizedBox(height:50),
-            ]
-        )),
-        width: (0.0), //?. : check if width exist, null-->not initialized, width=0.0, else return width 
+    return new Container(
+        child: new Center(
+            child: new Column(children: [
+          new SizedBox(height: 75),
+          new CircleAvatar(
+            child: new Icon(Icons.school),
+            radius: 70.0,
+          ),
+          new SizedBox(height: 35),
+          new Text(
+            element,
+            textAlign: TextAlign.center,
+            textScaleFactor: 2.0,
+          ),
+          new SizedBox(height: 50),
+        ])),
+        width:
+            (0.0), //?. : check if width exist, null-->not initialized, width=0.0, else return width
         height: double.infinity,
-        decoration: new BoxDecoration(
-          color: Colors.lightBlue[200]
-        ),
+        decoration: new BoxDecoration(color: Colors.lightBlue[200]),
         onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => CreateSurvey()),
-      );
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CreateSurvey()),
+          );
+        });
   }
-
-  
 }
