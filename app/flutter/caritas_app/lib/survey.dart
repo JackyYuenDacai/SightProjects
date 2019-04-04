61af9fd26394a1f9ba2f6b700a6c7ece5fbc6235
Deepskyhunter <48083317+Deepskyhunter@users.noreply.github.com>
2019-04-04 23:47:12 +0800

    Update survey.dart

--- app/flutter/caritas_app/lib/survey.dart
+++ app/flutter/caritas_app/lib/survey.dart
@@ -18,31 +18,39 @@ class CreateSurvey extends StatefulWidget {
   }
 }
 class CreateSurveyState extends State<CreateSurvey> {
+  String answer;
+  surveyAnswer(String answer){
+    this.answer = answer;
+  }
   get name => null;
   final List questionList = ['Who are you?','How old are you?','Where will you go?'];
   final List answerList = ['A','B','C','D'];
   @override
   Widget build(BuildContext context) {
-    Widget surveyQuestion =(
-      Container(
-        
-      )
-
-    );
-    Widget surveyAnswer =(
-      Container(
-
-      )
-
-    );
     return Scaffold(
         appBar: AppBar(title: name,),
-        body: Column(
-          children: <Widget> [
-            surveyQuestion,
-            surveyAnswer
+        body: Row(
+          children: [
+            Survey.question(questionList[0]),
+            Row(children: answerList.map((element) => Survey.answer(element)).toList())
           ]
         ),
+    );
+  }
+}
+class Survey extends StatelessWidget {
+  String q;
+  Survey.question(String q){
+    this.q = q;
+  }
+  String a;
+  Survey.answer(String a){
+    this.a = a;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(a)
     );
   }
 }
