8b35463b877ae57826de5995c7707e8ded3ebcae
don11669 <48054558+don11669@users.noreply.github.com>
2019-05-05 19:02:42 +0800

    Add actual tags to init-sample-data

--- server/mysql-script/init-sample-data.sql
+++ server/mysql-script/init-sample-data.sql
@@ -3,6 +3,17 @@ insert into personnel values("111",'Staff Main',0,null);
 insert into personnel values('001','John',1,null);
 insert into personnel values('002','Don',1,null);
 insert into personnel values('003','Jack',1,null);
+
+insert into personnel values('9662AZ','Mickey',1,null);
+insert into personnel values('9664AZ','Don',1,null);
+insert into personnel values('9663AZ','Jacky',1,null);
+insert into personnel values('9665AZ','StaffJackey',0,null);
+
+insert into tags_linkage values('9662AZ','E2-00-00-1B-89-13-01-83-05-00-9E-9F');
+insert into tags_linkage values('9664AZ','E2-00-00-1B-89-13-02-07-05-00-B7-BB');
+insert into tags_linkage values('9663AZ','E2-00-00-1B-89-13-02-08-05-00-B6-E8');
+insert into tags_linkage values('9665AZ','E2-00-00-1B-89-13-01-81-05-00-9E-9E');
+
 /*CHANGE TO LINKAGE*/
 /*
 insert into pers onnel values("01-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00",'root',0,null);
